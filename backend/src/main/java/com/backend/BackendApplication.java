c68af42475a0cc7a9a572a76f2a84ebb076c92db
Francesco <142682839+FraLobbia@users.noreply.github.com>
2024-08-22 11:11:02 +0200

    fix docker env and script
    aggiunto --host 0.0.0.0 a script per avviare frontend correttamente

--- backend/src/main/java/com/backend/BackendApplication.java
+++ backend/src/main/java/com/backend/BackendApplication.java
@@ -1,7 +1,5 @@
 package com.backend;
 
-import org.hibernate.HibernateException;
-import org.hibernate.cfg.Configuration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -19,11 +17,6 @@ public class BackendApplication {
 	public static void main(String[] args) {
 
 		try {
-			Configuration configuration = new Configuration();
-			configuration.configure("hibernate.cfg.xml");
-			configuration.buildSessionFactory();
-		} catch (HibernateException e) {
-			logger.error("HibernateException occurred", e);
 		} catch (Exception e) {
 			logger.error("Exception occurred", e);
 		}
