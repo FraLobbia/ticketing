a4e1ff0db33662e6c269254fb71497a33d80d9ff
Francesco <142682839+FraLobbia@users.noreply.github.com>
2025-01-06 16:06:28 +0100

    added selenium in pom

--- backend/src/main/java/com/backend/security/JwtTokenProvider.java
+++ backend/src/main/java/com/backend/security/JwtTokenProvider.java
@@ -1,8 +1,8 @@
 package com.backend.security;
 
+import java.nio.file.Paths;
 import java.security.Key;
 import java.util.Date;
-
 import javax.crypto.SecretKey;
 
 import org.springframework.security.core.Authentication;
@@ -16,32 +16,80 @@ import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.JwtParserBuilder;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
 
+/**
+ * Classe JwtTokenProvider responsabile della generazione e validazione dei
+ * token JWT.
+ * Utilizza una chiave segreta caricata da un file .env per firmare i token.
+ * 
+ * <p>
+ * Metodi principali:
+ * </p>
+ * <ul>
+ * <li>{@link #generateToken(Authentication)}: Genera un token JWT per un utente
+ * autenticato.</li>
+ * <li>{@link #getUsernameFromToken(String)}: Estrae il nome utente (subject) da
+ * un token JWT.</li>
+ * <li>{@link #validateToken(String)}: Verifica se un token JWT è valido.</li>
+ * </ul>
+ * 
+ * <p>
+ * Proprietà:
+ * </p>
+ * <ul>
+ * <li>jwtSecret: La chiave segreta per la firma del token JWT.</li>
+ * <li>jwtExpirationDate: La durata di validità del token JWT in
+ * millisecondi.</li>
+ * </ul>
+ * 
+ * <p>
+ * Costruttore:
+ * </p>
+ * <ul>
+ * <li>{@link #JwtTokenProvider()}: Carica la chiave segreta dal file .env.</li>
+ * </ul>
+ * 
+ * <p>
+ * Metodi privati:
+ * </p>
+ * <ul>
+ * <li>{@link #loadJwtSecretFromEnv()}: Carica la chiave segreta per la firma
+ * del token JWT dal file .env.</li>
+ * <li>{@link #getSigningKey()}: Restituisce la chiave di firma per JWT.</li>
+ * </ul>
+ * 
+ * <p>
+ * Eccezioni:
+ * </p>
+ * <ul>
+ * <li>{@link IllegalStateException}: Se il file .env non viene trovato o la
+ * chiave JWT_SECRET non è impostata.</li>
+ * </ul>
+ * 
+ * <p>
+ * Dipendenze:
+ * </p>
+ * <ul>
+ * <li>Dotenv: Per caricare le variabili d'ambiente dal file .env.</li>
+ * <li>Jwts: Per creare e analizzare i token JWT.</li>
+ * <li>Keys: Per generare la chiave di firma HMAC.</li>
+ * </ul>
+ */
+@Slf4j
 @Component
 public class JwtTokenProvider {
 
+  // Properties
   private final String jwtSecret;
+  private final long jwtExpirationDate = 3600000 * 24 * 7; // 1h = 3600s e 3600*1000 = 3600000 milliseconds
 
   /**
    * Costruttore della classe JwtTokenProvider.
-   * 
-   * Inizializza la variabile jwtSecret con il valore della variabile d'ambiente
-   * JWT_SECRET.
-   * Se la variabile d'ambiente JWT_SECRET non è impostata o è vuota, viene
-   * lanciata un'eccezione.
-   * 
-   * @throws IllegalStateException Se la variabile d'ambiente JWT_SECRET non è
-   *                               impostata o è vuota.
    */
   public JwtTokenProvider() {
-    Dotenv dotenv = Dotenv.load();
-    jwtSecret = dotenv.get("JWT_SECRET");
-    if (this.jwtSecret == null || this.jwtSecret.isEmpty()) {
-      throw new IllegalStateException("JWT_SECRET environment variable is not set or is empty");
-    }
+    this.jwtSecret = loadJwtSecretFromEnv();
   }
-
-  private final long jwtExpirationDate = 3600000 * 24 * 7; // 1h = 3600s e 3600*1000 = 3600000 milliseconds
 
   /**
    * Genera un token JWT per l'utente autenticato.
@@ -124,4 +172,48 @@ public class JwtTokenProvider {
     return Keys.hmacShaKeyFor(jwtSecret.getBytes());
   }
 
+  /**
+   * Carica la chiave segreta per la firma del token JWT dal file .env.
+   * Esegue 3 tentativi:
+   * 1. Carica il file .env dalla directory corrente. (se si avvia il progetto con
+   * docker-compose)
+   * 2. Carica il file .env dalla directory superiore. (se si avvia il progetto
+   * con mvn spring-boot:run)
+   * 3. Se non trova il file .env, lancia un'eccezione.
+   * 
+   * @return String La chiave segreta per la firma del token JWT.
+   */
+  private String loadJwtSecretFromEnv() {
+    Dotenv dotenv = null;
+
+    // Primo tentativo: directory corrente
+    try {
+      dotenv = Dotenv.configure().load();
+      log.info("Caricato file .env dalla directory corrente.");
+    } catch (Exception e) {
+      log.warn("File .env non trovato nella directory corrente. Tentativo con la directory superiore...");
+    }
+
+    // Secondo tentativo: directory superiore
+    if (dotenv == null) {
+      try {
+        String parentDir = Paths.get(".").toAbsolutePath().normalize().getParent().toString();
+        dotenv = Dotenv.configure().directory(parentDir).load();
+        log.info("Caricato file .env dalla directory superiore: " + parentDir);
+      } catch (Exception e) {
+        log.error("File .env non trovato nella directory superiore.");
+      }
+    }
+
+    if (dotenv != null) {
+      String jwtSecret = dotenv.get("JWT_SECRET");
+      if (jwtSecret == null || jwtSecret.isEmpty()) {
+        throw new IllegalStateException("JWT_SECRET non è impostata nel file .env.");
+      }
+      return jwtSecret;
+    } else {
+      throw new IllegalStateException(
+          "File .env non trovato né nella directory corrente né in quella superiore. Creare un file .env con property JWT_SECRET.");
+    }
+  }
 }
---
+++ backend/src/test/java/com/backend/selenium/LoginTest.java
@@ -0,0 +1,26 @@
+// package com.backend.selenium;
+
+// import org.junit.jupiter.api.Test;
+// import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.chrome.ChromeDriver;
+
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// public class LoginTest {
+
+// @Test
+// public void testLoginPage() {
+// // Imposta il percorso del driver di Chrome
+// System.setProperty("webdriver.chrome.driver",
+// "src/test/resources/drivers/chromedriver");
+
+// WebDriver driver = new ChromeDriver();
+// try {
+// driver.get("http://localhost:4200");
+// String title = driver.getTitle();
+// assertEquals("Login - Ticketing", title);
+// } finally {
+// driver.quit();
+// }
+// }
+// }
